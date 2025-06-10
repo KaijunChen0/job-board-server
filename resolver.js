e78e855e4121e54b0d736972d6db853227e62913
KaijunChen0 <113864900+KaijunChen0@users.noreply.github.com>
2025-06-09 23:37:24 -0700

    Arrays and non-nullability

--- resolver.js
+++ resolver.js
@@ -2,12 +2,19 @@ import { title } from "node:process";
 
 export const resolvers ={
     Query :{
-        job: () => {
-            return {
-              id: 'test-id',
-              title: 'The job',
+        jobs: () => {
+            return [
+            {
+              id: 'test-id-1',
+              title: 'The job1',
               description: "The description",
-            };
+            },
+            {
+              id: 'test-id-2',
+              title: 'The job2',
+              description: "The description",
+            },
+          ];
         },
     }
 }
